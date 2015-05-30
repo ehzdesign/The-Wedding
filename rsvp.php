b3800acb8289097c6a8deb72f028d7c922e54967
ehzdesign <91.eahz@gmail.com>
2015-05-29 22:16:37 -0400

    add doctor house images

--- rsvp.php
+++ rsvp.php
@@ -1,12 +1,20 @@
 <?php 
 $name = $_POST['name'];
 $attendancechoice = $_POST['attendancechoice'];
-$formcontent="From: $name \n Response: I $attendancechoice";
+$howmany = $_POST['how-many'];
+$plus1 = $_POST['plus1'];
+$guests = $_POST['guests'];
+$notes = $_POST['notes'];
+$email = $_POST['email'];
+
+$formcontent="From: $name \n Response: I $attendancechoice \n Guest Choice: $howmany \n Guest Name(s):$guests";
 $recipient = "91.eahz@gmail.com";
 $subject = "Wedding RSVP Form";
-$mailheader = "From: $name \r\n";
+$mailheader = "From: $name <$email>\r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
 echo "Thank You!";
+
+
 ?>
 
 
