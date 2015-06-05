e2fc3d7f5061554d36ab3455e7bd1873ba4d0be5
ehzdesign <91.eahz@gmail.com>
2015-06-05 18:14:11 -0400

    rsvp form now working with mailchimp

--- js/script.js
+++ js/script.js
@@ -12,8 +12,8 @@ $('#mainNav a, #countdown a').smoothScroll();
 //==========================================
 
 
- $('.clock').countdown('2015/09/14', function(event) {
-   
+$('.clock').countdown('2015/09/14', function(event) {
+
    //display clock in : w/d/hr/min/sec format
    var $this = $(this).html(event.strftime(''
      + '<span>%w</span> weeks '
@@ -21,11 +21,11 @@ $('#mainNav a, #countdown a').smoothScroll();
      + '<span>%H</span> hr '
      + '<span>%M</span> min '
      + '<span>%S</span> sec'));
-  
+
    // display message when countdown is complete
    $this.on('finish.countdown', function(){
     $('.clock').html('Yay! Today is the day!');
-   });
+  });
  });
 
 
@@ -48,9 +48,9 @@ var waypoint = new Waypoint({
     $('.main-nav').fadeOut('fast');
   }
 
-    
-  },
-  offset: '10%'
+
+},
+offset: '10%'
 })
 
 
@@ -60,7 +60,7 @@ var waypoint = new Waypoint({
 //  var feed = new Instafeed({
 //         get: 'tagged',
 //         tagName: 'MyCuteNieceOlivia', 
-       
+
 //         clientId: '698a1ba199a648ceacf26e00e835f81a',
 //         limit:'14'
 //     });
@@ -87,10 +87,10 @@ $.ajax({
       // var $newImage = $('<img src="'+item.images.low_resolution.url+'">');      
       var $newImage = $('<a href="'+item.link+'"><img src="'+item.images.low_resolution.url+'"></a>');
       var $newImageSecondary = $('<a href="'+item.link+'"><img src="'+item.images.low_resolution.url+'"></a>');    
-        $('#instafeed').append($newImage);
-        $('#instafeed-secondary').append($newImageSecondary);
+      $('#instafeed').append($newImage);
+      $('#instafeed-secondary').append($newImageSecondary);
     });
-});;
+  });;
 
 
 
@@ -101,19 +101,22 @@ $.ajax({
 // Display inputs pertaining to proper radio button selected
 // =============================================================
 $('input[type=radio]').on('click', function(event) {
-  
+
   var $notAttendingBtn = $('#not-attending'),
-      $attendingBtn = $('#attending'),
-      $rsvpFormInfo = $('.rsvp__form__info'),
-      $plus1Input = $('#plus1__text-input'),
-      $plusFamilyInput = $('#family-member__input-section'),
-      $notesMessageAttending = 'Please let us know of any dietary restrictions or leave us a heartwarming message.',
-      $notesMessageNotAttending = 'Aww, that’s too bad but we still love you. Want to leave us a heartwarming message?';
+  $attendingBtn = $('#attending'),
+  $rsvpFormInfo = $('.rsvp__form__info'),
+  $plus1Input = $('#plus1__text-input'),
+  $plusFamilyInput = $('#family-member__input-section'),
+  $notesMessageAttending = 'Please let us know of any dietary restrictions or leave us a heartwarming message.',
+  $notesMessageNotAttending = 'Aww, that’s too bad but we still love you. Want to leave us a heartwarming message?';
 
   // reveal rsvp notes section if either attending or not attending radio button selected
   if(($attendingBtn).prop('checked') || ($notAttendingBtn).prop('checked')){
-    console.log('working');
+    console.log('attendancechoice made');
     $rsvpFormInfo.fadeIn('300');
+     $('body').animate({
+        scrollTop: $(".rsvp__form__info").offset().top
+    }, 500);
   }
 
   // actions when plus1 radio is selected
@@ -121,30 +124,30 @@ $('input[type=radio]').on('click', function(event) {
    $plus1Input.show(300, function(){
      console.log('plus1 radio button selected');
    });
-  } else{
-      $plus1Input.hide(300);
-  }
+ } else{
+  $plus1Input.hide(300);
+}
 
   // actions when plus family radio is selected
   if($('#plusfamily-radio').prop('checked')) {
    $plusFamilyInput.hide().show(300, function(){
      console.log('plus-family radio button selected'); 
    });
-  } else{
-      $plusFamilyInput.hide(300,function(){
-        $('.family-member__input').slice(1).remove();
-      });
-    }
+ } else{
+  $plusFamilyInput.hide(300,function(){
+    $('.family-member__input').slice(1).remove();
+  });
+}
 
-  
+
   // actions when 'will be there in spirit' (same as not attending) radio is selected
   if($notAttendingBtn.prop('checked')) {
     console.log('will be there in spirit is checked');
     $('.whos-attending').fadeOut(300);
     $plusFamilyInput.add($plus1Input).hide();
     $('.rsvp__form__info__notes__message').text($notesMessageNotAttending)
-      .hide()
-      .fadeIn(300);
+    .hide()
+    .fadeIn(300);
   }else{
     $('.whos-attending')
     .css({
@@ -185,8 +188,107 @@ $('.add-family-member-btn').on('click', function(event) {
 });
 
 
+// form submission (ajax request)
+// ======================================
+
+var $form = $('#rsvp__form');
+console.log($form.attr('action'));
+
+var $formMessages = $('#formMessages');
+var $formMessagesText = $('.formMessagesText');
+
+function successMessage(){
+
+  $formMessagesText.text('Whatever you did it was a success!')
+  $formMessagesText.addClass('successIcon');
+  $formMessages.addClass('success')
+  .css({
+   display: 'flex',
+   display: '-webkit-flex'
+ })
+  .hide()
+  .fadeIn('200')
+  .delay('2500')
+  .fadeOut('200');
+}
 
 
+function errorMessage(){
+  $formMessagesText.addClass('errorIcon');
+  $formMessages.addClass('error')
+  .css({
+   display: 'flex',
+   display: '-webkit-flex'
+ })
+  .hide()
+  .fadeIn('200')
+  .delay('2500')
+  .fadeOut('200');
+}
+
+
+var $form = $('#rsvp__form'); //get form
+
+
+$form.submit(function(event){
+
+
+  event.preventDefault();
+  var formData = $form.serialize();
+
+  $.ajax({
+    url: $form.attr('action'),
+    type: 'POST',
+    data: formData
+  })
+  .done(function(response) {  //what fires when submission is complete
+   var $submitFormBtn = $('#submitFormBtn');
+   console.log(response);
+
+   $submitFormBtn.prop('disabled', true);
+   
+   $form.trigger('reset'); //clear form fields after form submission
+   
+   $submitFormBtn.val('Thank You') //change submit button appearance
+   .removeClass('sendBtn')
+   .addClass('success');
+
+   successMessage();
+   
+
+   console.log("form submission success");
+    // make a formMessages div
+    // $(formMessages).removeClass('error');
+    // $(formMessages).removeClass('success');
+    // $(formMessages).text('response');
+
+    //clear all form inputs
+  })
+  .fail(function(data) {
+    console.log("error");
+    // $(formMessages).removeClass('success');
+    // $(formMessages).removeClass('error');
+    if(data.responseText !== ''){
+      console.log(data.status);
+      errorMessage();
+      if(data.status === 404){
+        $formMessagesText.text('Sorry but requested page cannot be found');
+      }else if(data.status === 500){
+        $formMessagesText.text('Server error please try again later');
+      }
+    }else{
+      errorMessage();
+      $formMessages.text('Oops! An error occured and your message could not be sent.');
+    }
+  })
+  .always(function() {
+    console.log("form submission complete");
+  });
+  
+  
+
+
+});
 
 
 
---
+++ rsvp_chimp.php
@@ -0,0 +1,91 @@
+<?php
+/* @var $c Mailchimp_Campaigns */
+/* @var $l Mailchimp_Lists */
+/* @var $t Mailchimp_Templates */
+/* @var $mc Mailchimp */
+
+require_once 'vendor/mailchimp/mailchimp/src/Mailchimp.php';
+$name= "";
+$attendancechoice= "";
+$whosattending= "";
+$notes= "";
+$email= "";
+
+
+if( isset($_POST['name']) )
+{
+  $name = $_POST['name'];
+}
+if( isset($_POST['attendancechoice']) )
+{
+  $attendancechoice = $_POST['attendancechoice'];
+}
+if( isset($_POST['how-many']) )
+{
+  $whosattending = $_POST['how-many'];
+}
+if( isset($_POST['notes']) )
+{
+  $notes = $_POST['notes'];
+}
+if( isset($_POST['email']) )
+{
+  $email = $_POST['email'];
+}
+
+$plus1 = 'n/a';
+
+switch($whosattending){
+  case 'plus 1': 
+    $plus1 = $_POST['plus-1'];
+    break;
+  case 'plus family':
+    $plusArray = $_POST['family-member'];
+    $plus1 = '<ul>';
+    foreach($plusArray as $familyMemberName){
+      $plus1.= '<li>'.$familyMemberName.'</li>';
+    }
+    $plus1.='</ul>';
+  break;
+}
+
+$message = file_get_contents('./mail.html');
+$message = str_replace('%name%', $name, $message);
+$message = str_replace('%attendancechoice%', $attendancechoice, $message);
+$message = str_replace('%how-many%', $whosattending, $message);
+$message = str_replace('%guestname%', $plus1, $message);
+$message = str_replace('%notes%', $notes, $message);
+$message = str_replace('%email%', $email, $message);
+
+
+
+
+
+/* --- CHIMP CONFIG --------------------------------------------------------- */
+
+$mc = new Mailchimp("5da02815b6068e788eddb64c34166506-us11");
+$c = $mc->campaigns;
+
+
+$type = "regular";
+
+$createCampainArray = array(
+   'list_id' => '9daa47dc5a',
+    'subject'=> 'The Wedding',
+    'from_email' => '91.eahz@gmail.com',
+    'from_name' => 'Chris and Lisa Wedding',
+    'to_name' => 'My BROTHER',
+    'inline_css' => true,
+    
+    
+);
+$content = array(
+    'html' => $message,
+);
+
+$r = $c->create($type, $createCampainArray, $content);
+
+var_dump($r['id']);   
+$r2 = $c->send($r['id']);
+
+var_dump($r2); 
